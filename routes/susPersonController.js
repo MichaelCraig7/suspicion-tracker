e983e520386cacbaecd140e6321db56a2cfec6d7
michaelcraig <m_craig@baylor.edu>
2018-06-07 15:03:42 -0400

    working on views, will get back to routes

--- routes/susPersonController.js
+++ routes/susPersonController.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('Who\'s sus?');
 });
+
+router.get('/new', (req, res) => {
+  res.render('susIndividual/new')
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.get('/:id/edit', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router;
--- routes/userController.js
+++ routes/userController.js
@@ -6,4 +6,28 @@ router.get('/', (req, res, next) => {
   res.send('yuh');
 });
 
+router.get('/new', (req, res) => {
+  res.render('user/new')
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.get('/:id/edit', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
 module.exports = router;
