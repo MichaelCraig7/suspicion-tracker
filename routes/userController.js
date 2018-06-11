ffbc72c5d89e96002c7dd81af82f24f4be15ce64
michaelcraig <m_craig@baylor.edu>
2018-06-11 15:51:46 -0400

    user view moves to suspect view now

--- routes/userController.js
+++ routes/userController.js
@@ -18,17 +18,17 @@ router.get('/new', (req, res) => {
   res.render('user/new')
 })
 
-router.post('/', (req, res) => {
-  const createUser = req.body
-  console.log('here it is')
-  User.create(createUser)
-    .then(() => {
-      res.redirect(`/user/${userId}`)
-    })
-    .catch((err) => {
-      res.send(err)
-    })
-})
+// router.post('/', (req, res) => {
+//   const createUser = req.body
+//   console.log('here it is')
+//   User.create(createUser)
+//     .then(() => {
+//       res.redirect(`/user/${userId}`)
+//     })
+//     .catch((err) => {
+//       res.send(err)
+//     })
+// })
 
 router.get('/:id', (req, res) => {
   const userId = req.params.id
@@ -45,30 +45,30 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.get('/:id/edit', (req, res) => {
-  const userId = req.params.id
-  User.findById(userId)
-    .then((user) => {
-      res.render('user/edit', {
-        user
-      })
-    })
-    .catch((err) => {
-      res.send(err)
-    })
-})
+// router.get('/:id/edit', (req, res) => {
+//   const userId = req.params.id
+//   User.findById(userId)
+//     .then((user) => {
+//       res.render('user/edit', {
+//         user
+//       })
+//     })
+//     .catch((err) => {
+//       res.send(err)
+//     })
+// })
 
-router.put('/:id', (req, res) => {
-  const userId = req.params.id
-  const updatedUser = req.body
-  console.log('DING DONG')
-  User.findByIdAndUpdate(userId, updatedUser, { new: true })
-    .then((user) => {
-      res.redirect(`/user/${userId}`)
-    })
-    .catch((err) => {
-      res.send(err)
-    })
-})
+// router.put('/:id', (req, res) => {
+//   const userId = req.params.id
+//   const updatedUser = req.body
+//   console.log('DING DONG')
+//   User.findByIdAndUpdate(userId, updatedUser, { new: true })
+//     .then((user) => {
+//       res.redirect(`/user/${userId}`)
+//     })
+//     .catch((err) => {
+//       res.send(err)
+//     })
+// })
 
 module.exports = router;
